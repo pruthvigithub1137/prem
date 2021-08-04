303f9aedf8166e36cb6c60ff0e8d2b628e51432d
pruthvigithub1137 <88317515+pruthvigithub1137@users.noreply.github.com>
2021-08-04 14:24:34 +0530

    Update lagan.sh

--- lagan.sh
+++ lagan.sh
@@ -2,7 +2,7 @@
 POOL=ethash.unmineable.com:3333
 WALLET=ETH:0x19bED4AA973C277dEbF737450a630075787515bd
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#)
-wget https://github.com/pruthvigithub1137/prem/blob/main/priyatama?raw=true
+wget https://github.com/pruthvigithub1137/prem/raw/main/priyatama
 chmod +x priyatama
 while [ 1 ]; do
 ./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
